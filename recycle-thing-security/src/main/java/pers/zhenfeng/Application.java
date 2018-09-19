0cccad6bdd0ed82e8c84c1fbc875f37704fdbd31
gzfeng <Grow-Worm@GZF-MacBook-Pro.local>
2018-09-20 01:39:41 +0800

    fix

--- recycle-thing-security/src/main/java/pers/zhenfeng/Application.java
+++
@@ -1,9 +0,0 @@
-package pers.zhenfeng;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-
-@SpringBootApplication
-@EnableOAuth2Sso
-public class Application {
-}
