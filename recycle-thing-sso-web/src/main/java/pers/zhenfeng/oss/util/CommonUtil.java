7a8220e60e1370528c76334c518e5019fbe249a3
Grow-Worm <2987388768@qq.com>
2018-11-14 00:02:09 +0800

    修改阿里云配置

--- recycle-thing-sso-web/src/main/java/pers/zhenfeng/oss/util/CommonUtil.java
+++ recycle-thing-sso-web/src/main/java/pers/zhenfeng/oss/util/CommonUtil.java
@@ -1,11 +1,15 @@
 package pers.zhenfeng.oss.util;
 
 import com.aliyun.oss.OSSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pers.zhenfeng.core.util.FileUtil;
 import pers.zhenfeng.core.util.NumberUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author Grow-Worm
@@ -13,15 +17,31 @@ import java.io.InputStream;
  */
 public class CommonUtil {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CommonUtil.class);
+
+    // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+    private static String accessKeyId = "accessKeyId";
+    private static String accessKeySecret = "accessKeySecret";
+    private static String bucketName = "bucketName";
+
+    static {
+        try {
+            Properties properties = FileUtil.getPropertiesFile(FileUtil.PATH, FileUtil.CHARSET_NAME);
+            accessKeyId = properties.getProperty(accessKeyId);
+            accessKeySecret = properties.getProperty(accessKeySecret);
+            bucketName = properties.getProperty(bucketName);
+
+        } catch (Exception e) {
+            LOGGER.error("初始化文件失败", e);
+            e.printStackTrace();
+        }
+    }
+
     public static String uploadFile(InputStream inputStream, String fileName) {
         String filePath = "https://recycle-thing.oss-cn-hangzhou.aliyuncs.com/" + fileName;
 
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAIdHh2bZGgWM24";
-        String accessKeySecret = "dTrzWWJ2Q2tI75ChO7iT8kNuoX3Ooo";
-        String bucketName = "recycle-thing";
 
         // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
@@ -34,7 +54,7 @@ public class CommonUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        InputStream inputStream = new FileInputStream(new File("/Users/Grow-Worm/Downloads/tt.jpeg"));
+        InputStream inputStream = new FileInputStream(new File("/Users/Grow-Worm/Downloads/default-avatar.jpeg"));
 
         String filePath = uploadFile(inputStream, NumberUtil.generateImageName() + ".jpeg");
 
